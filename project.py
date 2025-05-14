5a6a743d0f9b418fdc146895e1cdfafeca4f0d62
user <user@MBP-user.dlink>
2025-05-14 10:24:42 +0300

    Add gevent to requirements'

--- project.py
+++ project.py
@@ -3,6 +3,18 @@ import random
 from collections import Counter
 
 app = Flask(__name__)
+
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
            
 questions = {
 "q1": {
