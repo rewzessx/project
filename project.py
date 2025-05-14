ec1b7f9d6aaa93717d0b9fcec47d02f97b80d0b1
rewzessx <polinakalchevs@gmail.com>
2025-05-14 17:10:59 +0300

    Update project.py

--- project.py
+++ project.py
@@ -282,9 +282,9 @@ def test():
     return render_template("test.html", questions=questions, question_keys=question_keys)
 @app.route("/results")
 def results():
-    professions=
-  request.arg.getlist('professions')
-return render_template("results.html", professions=professions)
+	professions= 
+	request.arg.getlist('professions')
+    return render_template("results.html", professions_data=professions)
 
 if __name__ == "__main__":
     app.run(debug=True)
